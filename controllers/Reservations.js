f3ab10d11f3510712ac74edd0073ad8bf77a41a6
jackloosantamaria <jackloosantamaria@hotmail.com>
2025-02-05 21:53:46 -0500

    copied last changes

--- controllers/Reservations.js
+++ controllers/Reservations.js
@@ -3,36 +3,152 @@ const ObjectId = require('mongodb').ObjectId;
 
 //get all Reservations
 const getAllReservation = async (req, res) => {
-    const result = await mongodb.getDatabase().db().collection('Reservations').find().toArray();
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(result);
-}
+  const result = await mongodb.getDatabase().db().collection('reservations').find().toArray();
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).json(result);
+};
 
 //get Reservation by Id
-
-const getReservationById = async(req, res) => {
-    const reservationsId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('Reservations').findOne({_id: reservationsId});
-    if (result){
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(result);
-    } else {
-        res.status(404).json({error: 'Reservation not found.'})
-    }
-}
-
-//get reservation by client ID
-
-const getReservationByClientId = async (req, res) => {
-    const clientId = req.params.clientId;
-    const result = await mongodb.getDatabase().db().collection('Reservations').find({clientId}).toArray();
-
+const getReservationById = async (req, res) => {
+  const reservationsId = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDatabase()
+    .db()
+    .collection('reservations')
+    .findOne({ _id: reservationsId });
+  if (result) {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(result);
-}
+  } else {
+    res.status(404).json({ error: 'Reservation not found.' });
+  }
+};
+
+//get reservation by client ID
+const getReservationByClientId = async (req, res) => {
+  const clientId = req.params.clientId;
+  const result = await mongodb
+    .getDatabase()
+    .db()
+    .collection('reservations')
+    .find({ clientId })
+    .toArray();
+
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).json(result);
+};
+
+// Add a new reservation
+const addReservation = async (req, res) => {
+  try {
+    const { clientId, roomType, checkInDate, checkOutDate, status, totalPrice, paymentStatus } =
+      req.body;
+
+    if (
+      !clientId ||
+      !roomType ||
+      !checkInDate ||
+      !checkOutDate ||
+      !status ||
+      !totalPrice ||
+      !paymentStatus
+    ) {
+      return res.status(400).json({ error: 'All fields are required.' });
+    }
+
+    const reservationData = {
+      clientId: new ObjectId(clientId),
+      roomType,
+      checkInDate: new Date(checkInDate),
+      checkOutDate: new Date(checkOutDate),
+      status,
+      totalPrice,
+      paymentStatus
+    };
+
+    const result = await mongodb
+      .getDatabase()
+      .db()
+      .collection('reservations')
+      .insertOne(reservationData);
+    res
+      .status(201)
+      .json({ message: 'Reservation added successfully', reservationId: result.insertedId });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to add reservation', details: error.message });
+  }
+};
+
+// Update a reservation
+const updateReservation = async (req, res) => {
+  try {
+    const reservationId = new ObjectId(req.params.id);
+    const { clientId, roomType, checkInDate, checkOutDate, status, totalPrice, paymentStatus } =
+      req.body;
+
+    if (
+      !clientId &&
+      !roomType &&
+      !checkInDate &&
+      !checkOutDate &&
+      !status &&
+      !totalPrice &&
+      !paymentStatus
+    ) {
+      return res.status(400).json({ error: 'At least one field must be provided for update.' });
+    }
+
+    //update object
+    const updateData = {};
+
+    if (clientId) updateData.clientId = new ObjectId(clientId);
+    if (roomType) updateData.roomType = roomType;
+    if (checkInDate) updateData.checkInDate = new Date(checkInDate);
+    if (checkOutDate) updateData.checkOutDate = new Date(checkOutDate);
+    if (status) updateData.status = status;
+    if (totalPrice) updateData.totalPrice = Number(totalPrice);
+    if (paymentStatus) updateData.paymentStatus = paymentStatus;
+
+    const result = await mongodb
+      .getDatabase()
+      .db()
+      .collection('reservations')
+      .updateOne({ _id: reservationId }, { $set: updateData });
+
+    if (result.matchedCount === 0) {
+      return res.status(404).json({ error: 'Reservation not found.' });
+    }
+
+    res.status(200).json({ message: 'Reservation updated successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to update reservation', details: error.message });
+  }
+};
+
+//Delete reservation
+
+const deleteReservation = async (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid reservation id to delete it');
+  }
+  const reservationId = new ObjectId(req.params.id);
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection('reservations')
+    .deleteOne({ _id: reservationId });
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error ocurred while deleting the reservation');
+  }
+};
 
 module.exports = {
-    getAllReservation,
-    getReservationById,
-    getReservationByClientId
-}
+  getAllReservation,
+  getReservationById,
+  getReservationByClientId,
+  addReservation,
+  updateReservation,
+  deleteReservation
+};
--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -11,20 +11,47 @@ const restaurantsController = require('../controllers/Restaurants');
 
 router.get('/clients', clientsController.getClients);
 router.get('/clients/:id', clientsController.getClientsById);
-
+router.delete('/clients/:id', clientsController.deleteClient);
 
 //Routes for activities
 router.get('/activities', activitiesController.getAllActivities);
 router.get('/activities/:id', activitiesController.getActivityById);
+router.delete('activities/:id', activitiesController.deleteActivity);
 
 //Routes for reservations
 router.get('/reservations', reservationsController.getAllReservation);
 router.get('/reservations/:id', reservationsController.getReservationById);
 router.get('/reservations/client/:clientId', reservationsController.getReservationByClientId);
+router.delete('/reservations/:id', reservationsController.deleteReservation);
 
 //Routes for restaurants
-router.get('/restaurants', restaurantsController);
+router.get('/restaurants', restaurantsController.getAllRestaurants);
 router.get('/restaurants/:id', restaurantsController.getRestaurantById);
 router.get('/restaurants/reservations', restaurantsController.getRestaurantReservations);
+router.get('/restaurants/:id', restaurantsController.deleteRestaurant);
+
+// POST to add a new activity
+router.post('/activities', activitiesController.addActivity);
+
+// PUT to update an activity
+router.put('/activities/:id', activitiesController.updateActivity);
+
+// POST to add a new client
+router.post('/clients', clientsController.addClient);
+
+// PUT to update a client
+router.put('/clients/:id', clientsController.updateClient);
+
+// POST to add a new reservation
+router.post('/reservations/', reservationsController.addReservation);
+
+// PUT to update a reservation
+router.put('/reservations/:id', reservationsController.updateReservation);
+
+// POST to add a new restaurant
+router.post('/restaurants/', restaurantsController.addRestaurant);
+
+// PUT to update a restaurant
+router.put('/restaurants/:id', restaurantsController.updateRestaurant);
 
 module.exports = router;
