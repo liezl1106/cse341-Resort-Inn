d92d4f591fd69fcd411d05e67fb968b2a63afb64
Sogiefa-12 <131720010+Sogiefa-12@users.noreply.github.com>
2025-02-13 14:54:07 +0100

    commit

--- middlewares/joi.js
+++ middlewares/joi.js
@@ -1,14 +1,30 @@
 const Joi = require('joi');
 
 const joiValidation = (schema) => {
-  return (req, res, next) => {
-    const { error, value } = schema.validate(req.body, {abortEarly: false});
-    if (error) {
-        return res.status(400).json({error: error.details.map(detail => detail.message)});    
+  return async (req, res, next) => {
+    try {
+      console.log('Validating request:');
+      console.log('Params:', req.params);
+      console.log('Body:', req.body);
+      console.log('Query:', req.query);
+      
+      const value = await schema.validateAsync({
+        body: req.body,
+        query: req.query,
+        params: req.params
+      });
+      
+      console.log('Validation successful:', value);
+      next();
+    } catch (error) {
+      console.log('Validation error:', error.message);
+      return res.status(400).json({
+        success: false,
+        message: error.message
+      });
     }
-    req.body = value;
-    next();
   };
 };
 
 module.exports = { joiValidation };
+;
