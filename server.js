0d9ab05c2c9ffa9e0f9c0332caacf34e620cbe5e
boluwatifeomotoyinbo <omotoyinbobade15@gmail.com>
2025-02-16 04:55:11 +0100

    auth

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('auth/github',
 );
 
 app.get(
-    'auth/github/callback',
+    '/auth/github/callback',
     passport.authenticate('github', {
       failureRedirect: '/api-docs',
       session: false,
